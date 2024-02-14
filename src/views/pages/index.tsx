55f848277458c10ad0968659e8b933915c957144
fatemeh <gholizadefatemeh37@gmail.com>
2024-02-14 23:40:18 +0330

    add main page actions

--- src/views/pages/index.tsx
+++ src/views/pages/index.tsx
@@ -1,10 +1,10 @@
 
 import { Route, Routes } from "react-router-dom";
-import Home from "./home";
+import MainPage from "./mainPage";
 const Pages = () => {
   return (
         <Routes >
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<MainPage />} />
         </Routes>
   );
 }
---
+++ src/views/pages/mainPage/index.tsx
@@ -0,0 +1,77 @@
+
+import { CustomButton, InputIcon, InputTextLabel } from "../../components";
+import { useState } from "react";
+import { TableProps } from "../../../types";
+import * as yup from 'yup'
+export function MainPage({}) {
+  const [ data, setData] = useState<TableProps>({rows: "0", columns: "0"});
+  const [errorsList, setErrorsList] = useState<Array<any>>([]);
+  const [dataIsSet, setDataIsSet] = useState<boolean>(false);
+  async function handleValidation(){
+    setErrorsList([])
+    const dataSchema = yup.object().shape({
+      data: yup.object().shape({
+        rows:yup.string().required()
+        .test('is-number-odd-greater-than-3', 'مقدار سطر باید یک عدد فرد و بزرگ‌تر یا مساوی با ۳ باشد.', (value) => {
+          if (!value) {
+            return false;
+          }    
+          const number = parseInt(value, 10);   
+          return !isNaN(number) && number % 2 === 1 && number >= 3;
+        }),
+        columns:yup.string().required()
+        .test('is-number-odd-greater-than-3', 'مقدار ستون باید یک عدد فرد و بزرگ‌تر یا مساوی با ۳ باشد.', (value) => {
+          if (!value) {
+            return false;
+          }    
+          const number = parseInt(value, 10);   
+          return !isNaN(number) && number % 2 === 1 && number >= 3;
+        }),        
+      })
+  });
+    try {
+      await dataSchema.validate({data}, { abortEarly: false }); 
+      setDataIsSet(true) 
+  }
+    catch (err: any) {
+      setErrorsList(err.inner);   
+      console.log("err", err.inner)
+ 
+  }
+}
+  return (
+    <div className="main-page">
+      <div className="card">
+        {!dataIsSet ? 
+        <>
+        <div className="inputs-container">
+          <div className="column-50">
+            <InputIcon name="row" myStyle="padd-l-10" value={data.rows} 
+            onChange={(e) => {setData((prev:TableProps) => ({...prev, rows: e.target.value}))}}
+            title="سطر" inBox="سطر" type="text" 
+            errortext={errorsList && errorsList?.filter((obj: any) => obj.path.includes(`data.rows`))[0]?.message}
+            />
+          </div>
+          <div className="column-50">
+            <InputIcon myStyle="w-50" name="row" value={data.columns} 
+            onChange={(e) => {setData((prev:TableProps) => ({...prev, columns: e.target.value}))}}
+            title="ستون" inBox="ستون" type="text" 
+            errortext={errorsList && errorsList?.filter((obj: any) => obj.path.includes(`data.columns`))[0]?.message}
+            />
+          </div>
+        </div>
+        <div className="button-container">
+          <CustomButton title="بعدی" onClick={() => handleValidation()} />
+        </div>
+        </>
+        :
+        <div></div>
+        
+}
+
+      </div>
+
+    </div>
+    );
+}
+export default MainPage;

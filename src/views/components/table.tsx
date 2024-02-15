61fb4de03e8e167db815dbebc7e2e46767e3d81c
fatemeh <gholizadefatemeh37@gmail.com>
2024-02-15 21:23:45 +0330

    handle NaN data input

--- src/views/components/table.tsx
+++ src/views/components/table.tsx
@@ -2,26 +2,36 @@ import React, { useState } from 'react';
 import { CustomButton, InputIcon } from '.';
 import { TableProps } from '../../types';
 import { generateTable } from '../../core/utils';
+import { FaSortAmountUpAlt } from "react-icons/fa";
+import * as yup from 'yup'
 
 const Table: React.FC<TableProps>= ({ rows, columns }) => {
   const [data, setData] = useState<number[][]>(generateTable(parseInt(rows), parseInt(columns)));
   const [sortedData, setSortedData] = useState<number[][]>(data);
+  const [errorsList, setErrorsList] = useState<Array<any>>([]);
   const handleCellChange = (event: React.ChangeEvent<HTMLInputElement>, rowIndex: number, columnIndex: number) => {
     setData((prevData) => {
       const newData = [...prevData];
-      newData[rowIndex][columnIndex] = parseInt(event.target.value, 10);
-      return newData;
+      const newValue = parseInt(event.target.value, 10);
+      if (!isNaN(newValue)) {
+        newData[rowIndex][columnIndex] = parseInt(event.target.value, 10);
+      }
+      else {
+        newData[rowIndex][columnIndex] = 0;
+      }
+      return newData
+      
     });
   };
   const handleSortClick = () => {
-    const sortedArray = data.map((row, index) => {
-      if (index % 2 === 0) {
-        return row.sort((a, b) => b - a); 
-      } else {
-        return row.sort((a, b) => a - b); 
-      }
-    });
-    setSortedData(sortedArray);
+      const sortedArray = data.map((row, index) => {
+        if (index % 2 === 0) {
+          return row.sort((a, b) => b - a); 
+        } else {
+          return row.sort((a, b) => a - b); 
+        }
+      });
+      setSortedData(sortedArray);
   };
   return (
     <div className='table-container'>
@@ -39,7 +49,8 @@ const Table: React.FC<TableProps>= ({ rows, columns }) => {
           ))}
         </tbody>
       </table>
-      <CustomButton title="مرتب‌سازی" onClick={handleSortClick} />
+      <span className='error-text'> {errorsList?.filter((obj: any) => obj.path.includes(`data`))[0]?.message}</span>
+      <CustomButton icon={<FaSortAmountUpAlt />} title="مرتب‌سازی" onClick={handleSortClick} />
     </div>
   );
 };

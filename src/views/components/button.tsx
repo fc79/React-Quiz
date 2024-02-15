8cc060203ff685f0dad9638c405798643f6b05ba
fatemeh <gholizadefatemeh37@gmail.com>
2024-02-15 20:43:46 +0330

    remove unused components

--- src/views/components/button.tsx
+++ src/views/components/button.tsx
@@ -1,23 +1,15 @@
-import {
-  ReactElement,
-  useCallback,
-  MouseEvent,
-  ButtonHTMLAttributes,
+import { ReactElement, useCallback, MouseEvent, ButtonHTMLAttributes
 } from "react";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   title: string;
   icon?:any;
-  route?:string;
   myStyle?:string;
-  loading?:boolean
   disabled?:boolean;
   textStyle?:string;
 }
 
-const ButtonComponent=({ onClick,loading, textStyle, icon, title,route , myStyle , disabled}: Props): ReactElement=> {
+const ButtonComponent=({ onClick, textStyle, icon, title , myStyle , disabled}: Props): ReactElement=> {
   const handleOnClick = useCallback(
     (event: MouseEvent<HTMLButtonElement>) => {
       event.preventDefault();
@@ -27,13 +19,12 @@ const ButtonComponent=({ onClick,loading, textStyle, icon, title,route , myStyle
   );
   return (
     <button
-      className={`button-component handle-loading-btn  ${myStyle}`}
+      className={`button-component handle-loading-btn ${myStyle}`}
       onClick={handleOnClick}
       disabled={disabled}
     >
       <h3 className={`button-component-text ${textStyle}`}>
         <div className="button-component-title">
-          {route && <Link to={route} target="_blank"></Link>}
            {icon && icon}
            {title}  
         </div>

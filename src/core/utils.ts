88e1f5549feed12c440268ab4480c531d0332cfb
fatemeh <gholizadefatemeh37@gmail.com>
2024-02-15 20:43:30 +0330

    add setting data function in utils

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,1 +1,11 @@
-export default {}
+export const generateTable = (rows: number, columns: number): number[][] => {
+    const table: number[][] = []; 
+    for (let i = 0; i < rows; i++) { 
+      const row: number[] = [];
+      for (let j = 0; j < columns; j++) {
+        row.push(0); 
+      } 
+      table.push(row);
+    } 
+    return table;
+  };
